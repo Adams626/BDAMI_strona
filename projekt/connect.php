df75b74a4fc3aa4da541a30a757cd4ce00fbb912
Adam Garncarz <83000803+Adams626@users.noreply.github.com>
2022-02-01 14:09:20 +0100

    Update connect.php

--- projekt/connect.php
+++ projekt/connect.php
@@ -1,8 +1,8 @@
 <?php
 
     $host = "localhost";
-    $db_user = "id18242744_admin";
-    $db_password = "!6SbN]1r/imRRO%!";
-    $db_name = "id18242744_portal";
+    $db_user = "";
+    $db_password = "";
+    $db_name = "";
 
 ?>
